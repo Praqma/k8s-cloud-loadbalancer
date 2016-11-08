d69385fb9b26dac769dd91e4925d2b999e9c6032
Kamran Azeem <kaz@praqma.net>
2016-11-08 11:43:10 +0100

    Disabled apache related lines.

--- lb-nodeport/run.sh
+++ lb-nodeport/run.sh
@@ -2,14 +2,23 @@
 source tools.f
 
 echo " - Running createLoadBalancer"
-createLoadBalancer
+createLoadBalancer haproxy default
 
-echo " - Cleaning up old files"
-rm -f /etc/httpd/conf.d/*.bl
+# echo " - Cleaning up old files"
+# rm -f /etc/httpd/conf.d/*.bl
 
-echo " - Copying files"
-mv -f kubernetes.services.conf /etc/httpd/conf.d/
-mv -f *.service.bl /etc/httpd/conf.d/
+# echo " - Copying files"
+# mv -f kubernetes.services.conf /etc/httpd/conf.d/
+# mv -f *.service.bl /etc/httpd/conf.d/
 
-echo " - Restarting httpd"
-sudo service httpd reload
+# echo " - Restarting httpd"
+# sudo service httpd reload
+
+echo "Restarting haproxy"
+sudo systemctl restart haproxy 
+
+# cp -f haproxy.cfg /etc/haproxy/
+
+sleep 2
+
+sudo systemctl status haproxy
