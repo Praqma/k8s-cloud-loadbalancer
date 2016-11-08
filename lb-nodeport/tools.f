21ff9682b880b17233a2059380a782265245e96f
Kamran Azeem <kaz@praqma.net>
2016-11-08 11:44:45 +0100

    removed apache sections and adjusted some areas.

--- lb-nodeport/tools.f
+++ lb-nodeport/tools.f
@@ -1,24 +1,37 @@
+# It is important to load apireader functions, which are used in this file.
 source ../apiReader/apiReader.f
 
 function createLoadBalancer(){
   local LBType=$1
+  local NameSpace=$2
 
-  # Setting LBType to apache, if not specified
+  # Setting LBType to haproxy - if not specified explicitly. 
+  # Apache works in a different way when used as proxy (as it is only a web proxy), so it is not recommended anyway.
   if [ -z "$LBType" ];then
      echo "Setting HAProxy as Loadbalancer engine (default)"
      LBType="haproxy"
   fi
 
-  if [ "$LBType" = "apache" ]; then
-    createLBApache
+
+  if [ -z "$NameSpace" ];then
+     echo "No NameSpace specified. Setting NameSpace to 'default'."
+     NameSpace="default"
   fi
-  
+
   if [ "$LBType" = "haproxy"  ]; then
-    createLBHaproxy
+    echo "Executing: createLBHaproxy ${NameSpace}"
+    createLBHaproxy ${NameSpace}
+  else
+    echo "Unknown LBType passed to this function (createoadBalancer). Please investigate."
   fi
 
 
-  # Remove the generated .cfg and .lb files
+
+  ## Copy the generated config file to /etc/haproxy
+  cp haproxy.cfg /etc/haproxy/haproxy.cfg
+
+
+  # Remove previously generated .cfg and .lb files
   # The default config file is haproxy.cfg.global-defaults , which will not be deleted by the commands below.
   rm -f *.cfg 
   rm -f *.lb 
@@ -26,9 +39,10 @@ function createLoadBalancer(){
 } 
 
 function createLBHaproxy(){
-  # Need to have a way to pass the name of namespace to this function,  if not "default"
+  # todo: Need to have a way to pass the name of namespace to this function,  if not "default"
 
-  local Services=$(getServices default | tr " " "\n")
+  local NameSpace=$1
+  local Services=$(getServices ${NameSpace} | tr " " "\n")
   local Nodes=$(getNodeNames)
   local nodeIP=""
   local line=""
@@ -37,141 +51,62 @@ function createLBHaproxy(){
 
   printf '%s\n' "$Services" | (while IFS= read -r line
   do
-    createServiceLBHaproxy "$line" "$Nodes" & 
-    done
-    wait
+    createServiceLBHaproxy "$line" "$NameSpace" "$Nodes" & 
+  done
+  wait
   )
 
 #  echo "<SERVICES>" >> haproxy.cfg
 
-  echo "listen stats *:1936
-    stats enable
-    stats uri /stats
-    stats hide-version
-    stats auth admin:Praqma" >> haproxy.cfg  
-
-    $(cat *.bl >> haproxy.cfg)
-    rm -f *.bl
+  cat *.lb >> haproxy.cfg
+  rm -f *.lb
 }
 
 function createServiceLBHaproxy(){
   local Service=$1
-  local Nodes=$(echo $2 | tr " " "\n")
+  local NameSpace=$2
+  local Nodes=$(echo $3 | tr " " "\n")
   local line=""
-  local i=0
+  local i=1
 
-  local ServicePort=$(getServiceNodePorts $Service "default")
-  local Endpoints=$(getServiceEndpoints $Service "default")
+  local NodePort=$(getServiceNodePorts $Service $NameSpace)
 
-  if [ ! "$ServicePort" == "null" ] && [ ! "$Endpoints" == "" ]; then
-    echo "
-frontend $Service
-    bind *:$ServicePort
-    mode http
-    default_backend "$Service"_BackEnds
-    " >> $Service.service.bl
+  # The following will give the IPs of the pods. (Needed to check if a service has endpoints or not).
+  local Endpoints=$(getServiceEndpoints $Service $NameSpace)
 
 
-    echo "backend "$Service"_BackEnds
-    mode http
-    balance roundrobin
-    option forwardfor
-    http-request set-header X-Forwarded-Port %[dst_port]
-    http-request add-header X-Forwarded-Proto https if { ssl_fc }
-    option httpchk HEAD / HTTP/1.1\r\nHost:localhost    " >> $Service.service.bl
+  local ServicePort=$(getServicePort $Service $NameSpace)
 
-    i=0
-    printf '%s\n' "$Nodes" | while IFS= read -r line
-    do
-      local nodeIP=$(getNodeIPs $line)
-      echo "    server "$Service"_node"$i" $nodeIP:$ServicePort check" >> $Service.service.bl
+  local ServiceFileName="${NameSpace}.${Service}.${ServicePort}.lb"
+
+  # Generate the listen section in haproxy for this particular service 
+  if [ ! "$NodePort" == "null" ] && [ ! -z "$Endpoints" ]; then
+    echo "Generating service related section in ${ServiceFileName} file ..."
+
+    ## Format:
+    ## listen apache
+    ##   bind 192.168.121.12:ServicePort
+    ##   server pod1 10.246.82.10:NodePort check
+    ##   server pod2 10.246.82.9:31380 check
+
+
+    echo "listen ${NameSpace}.${Service}.${ServicePort}"   >> ${ServiceFileName}
+    echo -e "\t bind *:${ServicePort}"    >> ${ServiceFileName} 
+    echo -e "\t option forwardfor"   >> ${ServiceFileName}
+
+    ## I am not sure about the following options. Do we need them in every service?
+    ## echo -e "\t http-request set-header X-Forwarded-Port %[dst_port]"    >> ${ServiceFileName}
+    ## echo -e "\t http-request add-header X-Forwarded-Proto https if { ssl_fc }"    >> ${ServiceFileName}
+    ## echo -e "\t option httpchk HEAD / HTTP/1.1\r\nHost:localhost"    >> ${ServiceFileName}
+
+    i=1
+    # printf '%s\n' "$Nodes" | while IFS= read -r line
+    for IP in $(getNodeIPs);  do
+      # local nodeIP=$(getNodeIPs $line)
+      echo -e "\t server Node_${i} ${IP}:${NodePort} check"    >> ${ServiceFileName}
       i=$((i+1))
     done
   fi
 
 }
 
-function createLBApache(){
-  local Services=$(getServices default | tr " " "\n")
-  local Nodes=$(getNodeNames)
-  local nodeIP=""
-  local line=""
-
-  echo "<VirtualHost *:80>
-        ProxyRequests off
-
-        ServerName example.org
-        ProxyPreserveHost On
-
-        IncludeOptional conf.d/*.bl" > kubernetes.services.conf
-
-
-  printf '%s\n' "$Services" | (while IFS= read -r line
-  do
-    createServiceLBApache "$line" "$Nodes" &
-    done
-    wait
-  )
-
-  echo "        # balancer-manager
-        # This tool is built into the mod_proxy_balancer
-        # module and will allow you to do some simple
-        # modifications to the balanced group via a gui
-        # web interface.
-        <Location /balancer-manager>
-                SetHandler balancer-manager
-
-                # I recommend locking this one down to your
-                # your office
-                # Require host example.org
-
-        </Location>
-
-        # Point of Balance
-        # This setting will allow to explicitly name the
-        # the location in the site that we want to be
-        # balanced, in this example we will balance "/"
-        # or everything in the site.
-        ProxyPass /balancer-manager !
-
-</VirtualHost>" >> kubernetes.services.conf
-
-}
-
-function createServiceLBApache(){
-  local Service=$1
-  local Nodes=$(echo $2 | tr " " "\n")
-  local line=""
-
-  local ServicePort=$(getServiceNodePorts $Service "default")
-  local Endpoints=$(getServiceEndpoints $Service "default")
-
-  if [ ! "$ServicePort" == "null" ] && [ ! "$Endpoints" == "" ]; then
-    echo "        <Proxy balancer://$Service>" > $Service.service.bl
-
-    printf '%s\n' "$Nodes" | while IFS= read -r line
-    do
-      local nodeIP=$(getNodeIPs $line)
-      echo "                BalancerMember http://$nodeIP:$ServicePort" >> $Service.service.bl
-    done
-
-    echo "
-                # Security technically we arent blocking
-                # anyone but this is the place to make
-                # those changes.
-                #Order Allow
-                #Require all granted
-                # In this example all requests are allowed.
-
-                # Load Balancer Settings
-                # We will be configuring a simple Round
-                # Robin style load balancer.  This means
-                # that all webheads take an equal share of
-                # of the load.
-                ProxySet lbmethod=byrequests
-        </Proxy>
-
-        ProxyPass /$Service balancer://$Service
-        ProxyPassReverse /$Service balancer://$Service" >> $Service.service.bl
-  fi
-}
